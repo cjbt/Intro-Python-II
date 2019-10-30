84a86171c7317459ecee7a9af6bf5d7008ae6b65
cecil john tantay <cjbtantay@gmail.com>
2019-10-30 13:33:54 -0700

    back to master

--- src/adv.py
+++ src/adv.py
@@ -1,18 +1,10 @@
-import sys
 from room import Room
-from item import Item
-from player import Player
 
 # Declare all the rooms
 
-s = Item("sword", "It's a weapon... use it.")
-gs = Item("greatsword", "It's a bigger weapon... use it.")
-d = Item("dagger", "It's a tiny weapon... use it.")
-b = Item("boot", "Someone's boot and it's not even a pair of boots.")
-
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons", [s, gs, d]),
+                     "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -26,7 +18,7 @@ to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""", [b]),
+earlier adventurers. The only exit is to the south."""),
 }
 
 
@@ -47,8 +39,6 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
-cj = Player('Cecil John', room['outside'], [])
-
 # Write a loop that:
 #
 # * Prints the current room name
@@ -59,70 +49,3 @@ cj = Player('Cecil John', room['outside'], [])
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
-
-# print(cj.current_room.name)
-# print(type(cj.current_room.n_to))
-# if (cj.current_room.s_to == False):
-#     print('hello')
-print("Welcome to this game, btw.")
-print("So youre cj.")
-print('Directions: n: north, s: south, e: east, w: west, and if youre a quitter, type quit')
-print("type 'o' to see check for items around the room")
-
-while True:
-    print('==========================================')
-    print('==========================================')
-    print(
-        f'Your current room is {cj.current_room.name}. {cj.current_room.description}. Around the room the items that are available are: {[str(i.name) for i in cj.current_room.items]}. What do you want to do or where do you want to go?')
-    current_room = input('Direction: ').strip().lower().split()
-    print('==========================================')
-
-    if len(current_room) == 2:
-        if current_room[0] == 'get' or current_room[0] == 'take':
-            item = [
-                i.name for i in cj.current_room.items if i.name == current_room[1]]
-            index = [i.name for i in cj.current_room.items]
-            indexed = item.index(item[0])
-            print(indexed)
-            if len(item) > 0:
-                current_item = item[0]
-                cj.inventory.append(current_item)
-                del cj.current_room.items[indexed]
-            else:
-                print('item not found')
-        else:
-            print('unknown commands')
-            break
-
-    if len(current_room) == 1:
-        if len(current_room[0]) > 1:
-            print('unknown commands')
-            break
-        if current_room[0] == 'q':
-            break
-        elif current_room[0] == 'n':
-            if hasattr(cj.current_room, 'n_to'):
-                cj.current_room = cj.current_room.n_to
-            else:
-                print('you cannot go there')
-        elif current_room[0] == 's':
-            if hasattr(cj.current_room, 's_to'):
-                cj.current_room = cj.current_room.s_to
-            else:
-                print('you cannot go there')
-        elif current_room[0] == 'e':
-            if hasattr(cj.current_room, 'e_to'):
-                cj.current_room = cj.current_room.e_to
-            else:
-                print('you cannot go there')
-        elif current_room[0] == 'o':
-            print([str(i.name) for i in cj.current_room.items])
-        elif current_room[0] == 'i':
-            print(f'Your inventory: {cj.inventory}')
-        elif current_room[0] == 'w':
-            if hasattr(cj.current_room, 'w_to'):
-                cj.current_room = cj.current_room.w_to
-            else:
-                print('you cannot go there')
-        else:
-            print('you cannot go there')
--- src/player.py
+++ src/player.py
@@ -1,12 +1,2 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-
-
-class Player:
-    def __init__(self, name, current_room, inventory=[]):
-        self.name = name
-        self.current_room = current_room
-        self.inventory = inventory
-
-    def __str__(self):
-        return f'Player(name: {self.name}, current_room: {self.current_room})'
